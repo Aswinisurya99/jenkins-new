ed7487134ed131c02df9127b2ac78d24ed662def
Aswinisurya99 <87408864+Aswinisurya99@users.noreply.github.com>
2022-01-24 22:19:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ec2_example" {
    ami           = "ami-08e4e35cccc6189f4"
    instance_type =  "t2.micro"
    count = 1
+   key_name= "isai"
 
    tags = {
            Name = "Terraform EC2"
